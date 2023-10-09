4ecbe5e19830ae1fc877ce0cf41e9eb5b086c5d7
helixplant <143057647+helixplant@users.noreply.github.com>
2023-10-08 23:58:46 -0400

    added js

---
+++ script.js
@@ -0,0 +1,26 @@
+$(window).scroll(function(e){
+    $el = $('.header');
+    $el.toggleClass('header_bar', $(this).scrollTop() > 0); 
+});
+
+var sections = $("section");
+	var navigation_links = $("#nav-wrap a");
+
+	sections.waypoint({
+
+      handler: function(event, direction) {
+
+		   var active_section;
+
+			active_section = $(this);
+			if (direction === "up") active_section = active_section.prev();
+
+			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+         navigation_links.parent().removeClass("current");
+			active_link.parent().addClass("current");
+
+		},
+		offset: '35%'
+
+	});
