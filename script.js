75d905830a79553b9d184a734472f9f0a52af85f
helixplant <143057647+helixplant@users.noreply.github.com>
2023-10-10 03:04:13 -0400

    almost done

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-$(window).scroll(function(e){
-    $el = $('.header');
-    $el.toggleClass('header_bar', $(this).scrollTop() > 0); 
-});
 
-var sections = $("section");
-	var navigation_links = $("#nav-wrap a");
 
-	sections.waypoint({
 
-      handler: function(event, direction) {
 
-		   var active_section;
-
-			active_section = $(this);
-			if (direction === "up") active_section = active_section.prev();
-
-			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
-
-         navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
-
-		},
-		offset: '35%'
-
-	});
+	function readMore(btn) {
+		let post = btn.parentElement;
+		let dots = post.querySelector(".dots");
+		let moreText = post.querySelector(".more");
+		
+		dots.classList.toggle("hide");
+		moreText.classList.toggle("hide");
+	  
+		btn.textContent = moreText.classList.contains("hide") ? "Read More" : "Read Less";
+	  }
+	  
